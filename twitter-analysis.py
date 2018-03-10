9338df10fd92301579fa4704b4583269ffc14955
Alice BC <alice.binghamcarr@gmail.com>
2018-03-10 15:18:34 +0000

    Add files via upload

--- twitter-analysis.py
+++ twitter-analysis.py
@@ -1,6 +1,5 @@
 import tweepy
 from textblob import TextBlob
-# import numpy as np
 import csv
 
 execfile("./environment.py")
@@ -13,9 +12,8 @@ api = tweepy.API(auth)
 public_tweets = api.search("brexit")
 
 for tweet in public_tweets:
-    print(tweet.text)
     analysis = TextBlob(tweet.text)
-    value = "negative" if analysis.sentiment.polarity <= 0 else "positive"
+    value = "negative" if analysis.sentiment.polarity < 0 else "positive" if analysis.sentiment.polarity > 0 else "neutral"
     with open("./csv/twitter/test.csv", "a") as f:
         writer = csv.writer(f)
         writer.writerow([tweet.text.encode("utf-8"), value])
